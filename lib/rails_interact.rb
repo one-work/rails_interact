7dc0943e001608763b962977024a5043da912b0b
qinmingyuan <mingyuan0715@foxmail.com>
2025-12-07 19:31:23 +0800

    auto commit

--- lib/rails_interact.rb
+++ lib/rails_interact.rb
@@ -1,2 +1,1 @@
 require 'rails_interact/engine'
-require 'rails_interact/config'
--- lib/rails_interact/config.rb
+++
@@ -1,9 +0,0 @@
-require 'active_support/configurable'
-
-module RailsInteract
-  include ActiveSupport::Configurable
-
-  configure do |config|
-  end
-
-end
